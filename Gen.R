058ee00c9721c76bf9fd62241f41b5527b483f69
Midou Seddik <78873358+medseddik1999@users.noreply.github.com>
2022-09-09 15:36:48 +0200

    Add files via upload

---
+++ Gen.R
@@ -0,0 +1,11 @@
+install.packages("pdftools")
+library(pdftools) 
+
+rmarkdown::render("reoprt.Rmd",
+                  output_file = "reoprt.pdf")
+
+
+pdf_combine(c("headt.pdf","Remerciem.pdf" , "reoprt.pdf",  "Appendix.pdf") , 
+            output = "SEDDIK_MOHAMED_M2_DS2E_2021-22_FV.pdf") 
+
+system('open "SEDDIK_MOHAMED_M2_DS2E_2021-22_FV.pdf"')
